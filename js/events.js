2106bf1bf9c068072981fe46951fd5573c0a4988
Davin Dillon <davindillon@gmail.com>
2017-09-19 21:28:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,7 +9,16 @@ function frameIt(){
     $('img').addClass('tasty');
   })
 }
-
+function submitIt(){
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
+function pressIt(){
+  $('input').on('keydown',function(){
+    
+  })
+}
 
 
 $(document).ready(function(){
